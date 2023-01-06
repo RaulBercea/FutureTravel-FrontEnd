51ba210302ea4b7b5ca224c9e293c042e299c676
RaulBercea <100358692+RaulBercea@users.noreply.github.com>
2023-01-06 21:24:06 +0100

    landing page completed

--- Future_Travel/src/app/app-routing.module.ts
+++ Future_Travel/src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingPageSwiperComponent } from './components/landing-page-swiper/landing-page-swiper.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
-const routes: Routes = [
-  { path: '', component: LandingPageComponent },
-  { path: 'test', component: LandingPageSwiperComponent },
-];
+const routes: Routes = [{ path: '', component: LandingPageComponent }];
 
 @NgModule({
   imports: [
