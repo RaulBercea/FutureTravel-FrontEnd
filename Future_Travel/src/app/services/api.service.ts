6fc96ce1af5e4c75c8418bf6aa23ab18947a6fbd
RaulBercea <raulbercea@gmail.com>
2023-01-20 14:46:48 +0100

    created the first api call

--- Future_Travel/src/app/services/api.service.ts
+++ Future_Travel/src/app/services/api.service.ts
@@ -12,13 +12,37 @@ export class apiService {
   // base url
   apiUrl: string = 'http://18.102.24.178:8085/gruppo8/front/';
 
-  getHotelLiokeData(province?: string, startDate?: string, endDate?: string) {
-    if (typeof province !== null) {
+  /**
+   * api call to get all of the hotellike data for the italian residents
+   * @param province - the istat province code (defaults to the regional code if not given)
+   * @param startDate - the begining date of the dataset (yyyy-mm format)
+   * @param endDate - the endingDate of the dataset (yyyy-mm format)
+   * @returns the resulting json of the api call
+   */
+  getHotelLikeItStoric(
+    province: string | 'ITF3',
+    startDate?: string,
+    endDate?: string
+  ) {
+    // check weather to add the date range to the api
+    if (typeof startDate !== null && typeof endDate !== null) {
       return this.httpClient.get(
-        `${this.apiUrl}/alloggio/hotellike?=${province}`
+        `${this.apiUrl}/S/IT/HOTELLIKE/${province}?startDate=${startDate}&endDate=${endDate}`
       );
-    } else {
-      return this.httpClient.get(`${this.apiUrl})`);
+    }
+    // individual checks for each of the ends of the range
+    else if (typeof startDate !== null && typeof endDate == null) {
+      return this.httpClient.get(
+        `${this.apiUrl}/S/IT/HOTELLIKE/${province}?startDate=${startDate}`
+      );
+    } else if (typeof startDate == null && typeof endDate !== null) {
+      return this.httpClient.get(
+        `${this.apiUrl}/S/IT/HOTELLIKE/${province}?endDate=${endDate}`
+      );
+    }
+    // return the result of the call with no query strings
+    else {
+      return this.httpClient.get(`${this.apiUrl}/S/IT/HOTELLIKE/${province}`);
     }
   }
 }
