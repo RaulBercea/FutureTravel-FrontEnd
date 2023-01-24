d9a7fcfcb14c86e76ea5b8a840c744969b096fc9
Francesco Abrate <francescoabrate21@gmail.com>
2023-01-24 13:32:55 +0100

    style and toggle function added

--- Future_Travel/src/app/components/navbar/navbar.component.ts
+++ Future_Travel/src/app/components/navbar/navbar.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  checkToogle() {
+    let navBarToggle = document.getElementById("navbar-default")
+    if (navBarToggle?.classList.contains("hidden")) {
+      navBarToggle?.classList.remove("hidden")
+    } else {
+      navBarToggle?.classList.add("hidden")
+    }
+
+  }
+
+
+
+
 
 }
