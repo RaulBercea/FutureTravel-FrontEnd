27491254218595f2c8dc63f022cd723c98b584dc
Francesco Abrate <francescoabrate21@gmail.com>
2023-01-25 23:00:52 +0100

    minor changes

--- Future_Travel/src/app/app-routing.module.ts
+++ Future_Travel/src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { ProvinceComponent } from './components/province/province.component';
-import { ProvincesPageComponent } from './components/provinces-page/provinces-page.component';
 import { AboutPageComponent } from './components/about-page/about-page.component';
 
 const routes: Routes = [{ path: '', component: LandingPageComponent },
-{ path: 'provinces-page', component: ProvincesPageComponent },
-{ path: 'province/:province', component: ProvinceComponent },
 { path: 'aboutus', component: AboutPageComponent }
 ];
 
--- Future_Travel/src/app/app.module.ts
+++ Future_Travel/src/app/app.module.ts
@@ -5,12 +5,10 @@ import { AppComponent } from './app.component';
 import { NgChartsModule } from 'ng2-charts';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { ProvincesPageComponent } from './components/provinces-page/provinces-page.component';
-import { ProvinceComponent } from './components/province/province.component';
 import { AboutPageComponent } from './components/about-page/about-page.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, LandingPageComponent, ProvincesPageComponent, ProvinceComponent, AboutPageComponent],
+  declarations: [AppComponent, NavbarComponent, LandingPageComponent, AboutPageComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
