f569f99ff539322f9acd415da35baa4e7f99e9e0
Francesco Abrate <francescoabrate21@gmail.com>
2023-01-26 20:31:43 +0100

    style changes

--- Future_Travel/src/app/app.module.ts
+++ Future_Travel/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { NgChartsModule } from 'ng2-charts';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { AboutPageComponent } from './components/about-page/about-page.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, LandingPageComponent, AboutPageComponent],
+  declarations: [AppComponent, NavbarComponent, LandingPageComponent, AboutPageComponent, FooterComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
