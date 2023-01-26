1b04b7dde36289ed7c47f3095bc1eedf6746dcb7
RaulBercea <100358692+RaulBercea@users.noreply.github.com>
2023-01-26 21:35:26 +0100

    Revert "Merge branch 'main' into chartDashboard"
    This reverts commit 2bf235757c82e89e98cedd1cdc42c0b496126b93, reversing
    changes made to e2330549f1a5eee1d654c4494591844b09575f8d.

--- Future_Travel/src/app/components/about-page/about-page.component.ts
+++ Future_Travel/src/app/components/about-page/about-page.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-about-page',
-    templateUrl: './about-page.component.html',
-    styleUrls: ['./about-page.component.scss']
+  selector: 'app-about-page',
+  templateUrl: './about-page.component.html',
+  styleUrls: ['./about-page.component.scss']
 })
 export class AboutPageComponent {
 
